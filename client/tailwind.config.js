b1dc758b175d392cc9cd9695d2de627bdc67fc15
mcdxwell <itsdavzz@gmail.com>
2022-01-21 15:15:09 -0600

    deleted

--- client/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  darkMode: true,
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
